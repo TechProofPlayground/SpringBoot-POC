9b424f114b41f8e302582a43091f02ce71432fa0
mayank <mayankkdb25@gmail.com>
2024-02-03 13:04:55 +0530

    getStudentBy Id

--- src/main/java/com/techproof/student/controller/StudentController.java
+++ src/main/java/com/techproof/student/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.techproof.student.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,11 @@ public class StudentController {
     public String student() {
         return "student";
     }
+
+    @GetMapping("/student/{id}")
+    public String Student(@PathVariable String id)
+    {
+        return student();
+    }
+
 }
